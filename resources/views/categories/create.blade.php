03c07d2fee86c2baf82440cfe9e617c8e05290c0
nezlicodes <benmeradi.lilyanezli@gmail.com>
2019-11-24 20:20:49 +0100

    updates create category view

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,3 +1,21 @@
 @extends('layouts.app')
 @section('content')
+<div class="content">
+    <div class="card">
+        <div class="card-header" style="background:white">
+            <h5 class="card-title">
+                Add Category:
+            </h5>
+        </div>
+        <div class="card-body">
+            <form method="post">
+                <div class="form-group">
+                    <label for="name" class="label">Category name:</label>
+                    <input type="text" name="name" placeholder="category name" class="form-control">
+                </div>
+                <a href="{{ route('categories.store') }}" type="submit" class="btn btn-success float-right">Add category</a>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
