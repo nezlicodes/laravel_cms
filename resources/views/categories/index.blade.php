35fc24d88e80e53e7782f88d31bdd6515e680daa
nezlicodes <benmeradi.lilyanezli@gmail.com>
2019-11-26 21:07:23 +0100

    wip delete with modeal

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -14,7 +14,8 @@
                 @foreach($categories as $category)
                 <li class="list-group-item"> {{ $category->name }}
                     <div class="d-flex justify-content-end align-content-center">
-                        <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-primary">edit</a>
+                        <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-primary mr-1">edit</a>
+                        <button class="btn btn-danger" type="button" data-toggle=" modal" data-target="#staticBackdrop">delete</button>
                     </div>
                 </li>
                 @endforeach
@@ -23,3 +24,23 @@
     </div>
 </div>
 @endsection
+
+<div class="modal fade" id="staticBackdrop" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Understood</button>
+            </div>
+        </div>
+    </div>
+</div>
