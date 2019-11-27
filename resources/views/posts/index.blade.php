259dcaf321a9a751c34aa67fdbfefe7b68911f44
nezlicodes <benmeradi.lilyanezli@gmail.com>
2019-11-27 06:13:00 +0100

    posts index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,19 +1,29 @@
 @extends('layouts.app')
 @section('content')
-
-<div class="d-flex flex-wrap justify-content-center align-content-center my-4">
-    @foreach($posts as $post)
-    <div class="card my-1 mr-3" style="max-width: 18rem;">
+<div class="content">
+    <div class=" d-flex justify-content-end mb-2">
+        <a href="{{ route('posts.create') }}" class="btn button-success col-md-2">Add a Post</a>
+    </div>
+    <div class="card">
         <div class="card-header" style="background:white">
-            <h5 class="card-title">{{ $post->title }}</h5>
+            <h5 class="card-title">Posts</h5>
         </div>
-        <div class="body p-2">
-            <p>
-                {{$post->content}}
-            </p>
+        <div class="card-body">
+            <div class="d-flex flex-wrap justify-content-center align-content-center my-4">
+                @foreach($posts as $post)
+                <div class="card my-1 mr-3" style="max-width: 18rem;">
+                    <div class="card-header" style="background:white">
+                        <h5 class="card-title">{{ $post->title }}</h5>
+                    </div>
+                    <div class="body p-2">
+                        <p>
+                            {{$post->content}}
+                        </p>
+                    </div>
+                </div>
+                @endforeach
+            </div>
         </div>
     </div>
-    @endforeach
 </div>
-
 @endsection
