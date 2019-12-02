925c180c951f79c5ff2ede36ee8c404916f69ce2
nezlicodes <benmeradi.lilyanezli@gmail.com>
2019-12-02 05:35:28 +0100

    request and request validation for posts

--- app/Http/Requests/Posts/Create.php
+++ app/Http/Requests/Posts/Create.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Posts;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CategoryRequest extends FormRequest
+class Create extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,10 +24,7 @@ class CategoryRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|unique:categories',
-            'content' => 'required',
-            'description' => 'required',
-            'image' => 'requried'
+            'title' => 'required'
         ];
     }
 }
